209057cb33aec71aac3a99512d7b0e7532047c5b
ersouravbansal <souravbansal277@gmail.com>
2022-07-09 14:44:42 +0530

    Updated Title and Meta Data

--- app/root.jsx
+++ app/root.jsx
@@ -8,7 +8,7 @@ import {
 } from "@remix-run/react";
 import indexstyle from "~/styles/index.css";
 import TopMenu from "./components/TopMenu";
-import MainNavigation from "./components/MainNavigation"
+import MainNavigation from "./components/MainNavigation";
 export const links = () => {
   return [
     {
@@ -20,8 +20,17 @@ export const links = () => {
 
 export const meta = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
+  title:
+    "Food News, Health News, Indian Recipes, Healthy Recipes, Vegetarian Recipes, Indian Food recipes – NDTV Food",
+  viewport:
+    "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no",
+  "apple-mobile-web-app-capable": "yes",
+  "apple-mobile-web-app-status-bar-style": "black-translucent",
+  "msapplication-tap-highlight": "no",
+  "google-site-verification": "yThRoDT_1iDUIum7IPIGT96Y-8rpvmFwlYBBK9EMGXM",
+  "Content-Type": "text/html; charset=UTF-8",
+  "content-language": "en",
+  "X-UA-Compatible": "IE=edge",
 });
 
 export default function App() {
